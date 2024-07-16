6e97b34e84b615a52fca25dd702c3cf66878d5c0
PRIYAM AGARWAL <95881431+PriyamAg001@users.noreply.github.com>
2024-07-16 21:35:03 +0530

    Create 15. 3 Sum (M) LeetCode.cpp

---
+++ Arrays/15. 3 Sum (M) LeetCode.cpp
@@ -0,0 +1,44 @@
+Code:
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+
+        vector<vector<int>> ans;
+        for(int i=0; i<n; i++){
+            if(i > 0 && nums[i] == nums[i-1]){
+                continue; // Skip duplicate elements
+            }
+            int j = i + 1;
+            int k = n - 1;
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum < 0){
+                    j++;
+                }
+                else if(sum > 0){
+                    k--;
+                }
+                else{
+                    vector<int> temp = {nums[i], nums[j], nums[k]};
+                    ans.push_back(temp);
+                    while(j < k && nums[j] == nums[j+1]){
+                        j++; // Skip duplicate elements
+                    }
+                    while(j < k && nums[k] == nums[k-1]){
+                        k--; // Skip duplicate elements
+                    }
+                    j++;
+                    k--;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+
+TC: O(N log N)
+SC: O(n^2)
