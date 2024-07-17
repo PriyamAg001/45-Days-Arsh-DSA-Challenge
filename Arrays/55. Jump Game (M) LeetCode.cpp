4588f75e67a3fce3ced587ed1fbb0f64d91f5e51
PRIYAM AGARWAL <95881431+PriyamAg001@users.noreply.github.com>
2024-07-17 19:28:47 +0530

    Create 55. Jump Game (M) LeetCode.cpp

---
+++ Arrays/55. Jump Game (M) LeetCode.cpp
@@ -0,0 +1,19 @@
+Code:
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int reachable = 0;
+        for(int i=0; i<n; i++){
+            if(i > reachable){
+                return false;
+            }
+            reachable = max(reachable, nums[i] + i);
+        }
+        return true;
+    }
+};
+
+TC: O(n)
+SC: O(1)
